0955799226cb2984d286eb1295b92734084721a4
Bryan <54081465+arthvadrr@users.noreply.github.com>
2026-07-07 19:31:57 -0400

    fix: Test still expecting an image

--- __tests__/components/DeckResults/DeckResultsView.test.tsx
+++ __tests__/components/DeckResults/DeckResultsView.test.tsx
@@ -8,7 +8,6 @@ import { DeckToTheGate } from '@/data/french/decks';
 import { router } from 'expo-router';
 import { useLinkProps } from '@react-navigation/native';
 import { fireEvent, render } from '@testing-library/react-native';
-import { ImageBackground } from 'react-native';
 
 /**
  * Mock the deck hook
@@ -104,7 +103,7 @@ describe('<DeckResultsView />', () => {
    * Make sure the results render
    */
   test('renders the deck details and correct and incorrect words', () => {
-    const { UNSAFE_getByType, getByText, getAllByText } = render(<DeckResultsView />);
+    const { getByText, getAllByText } = render(<DeckResultsView />);
 
     /**
      * Make sure the title row is rendering the selected deck.
@@ -114,11 +113,6 @@ describe('<DeckResultsView />', () => {
     getByText('Good job! You completed a ');
     expect(getAllByText(DeckToTheGate.title)).toHaveLength(2);
     getByText(' deck.');
-
-    /**
-     * Make sure the image source is the selected deck image.
-     */
-    expect(UNSAFE_getByType(ImageBackground).props.source).toBe(testingImage);
 
     /**
      * Make sure correct words render in the correct section.
