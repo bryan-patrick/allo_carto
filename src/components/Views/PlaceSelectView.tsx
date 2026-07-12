2c034bf36e8901323c18204f223dd19a755c29a8
Bryan <54081465+arthvadrr@users.noreply.github.com>
2026-07-12 15:46:36 -0400

    chore: condensed the file

--- src/components/Views/PlaceSelectView.tsx
+++ src/components/Views/PlaceSelectView.tsx
@@ -2,7 +2,7 @@ import { deckAtlas, DeckChapter, DeckPlace } from "@/data/french/deckAtlas";
 import sharedStyles from "@/src/app/sharedStyles";
 import { useUserContext } from "@/src/db/useUserContext";
 import { getDecksProgress } from "@/src/util/getDecksProgress";
-import { LinearGradient } from "expo-linear-gradient";
+import { LinearGradient, type LinearGradientProps } from "expo-linear-gradient";
 import { useFocusEffect, useLocalSearchParams } from "expo-router";
 import { useCallback, useState } from "react";
 import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
@@ -14,29 +14,50 @@ interface PlaceProgressById {
 }
 
 /**
+ * For the progress bars on the polaroids
+ */
+const polaroidColors: LinearGradientProps[ 'colors' ] = [
+  '#E6320D', '#E6320D',
+  '#F17E06', '#F17E06',
+  '#F8BA26', '#F8BA26',
+  '#78BA34', '#78BA34',
+  '#3791CF', '#3791CF'
+];
+
+const polaroidColorStops: NonNullable<LinearGradientProps[ 'locations' ]> = [
+  0, 0.2,
+  0.2, 0.4,
+  0.4, 0.6,
+  0.6, 0.8,
+  0.8, 1
+];
+
+/**
  * PlaceSelectView component
  */
 export default function PlaceSelectView() {
   const user = useUserContext();
   const [ placeProgressById, setPlaceProgressById ] = useState<PlaceProgressById>({});
+
+  /**
+   * DestructureStyles
+   */
   const {
-    chapterContainerStyle,
-    polaroid,
-    polaroidInnerStyle,
+    viewStyle,
     chapterTitleContainerStyle,
     chapterIndexStyle,
     chapterTitleStyle,
+    chapterContainerStyle,
     placeContainerStyle,
+    placeTitleTextStyle,
+    polaroidContainerStyle,
+    polaroid,
+    placeImageStyle,
+    placeDescriptionTextStyle,
     progressContainerStyle,
+    progressTextStyle,
     progressBarsContainer,
     progressBarStyle,
-    progressTextStyle,
-    polaroidContainerStyle,
-    placeImageStyle,
-    placeNameContainerStyle,
-    placeTitleContainerStyle,
-    placeTitleTextStyle,
-    placeDescriptionTextStyle,
     placeSelectButtonStyle,
     placeSelectButtonTextStyle
   } = styles;
@@ -83,94 +104,72 @@ export default function PlaceSelectView() {
    * Render the card grid
    */
   return (
-    <View style={{ backgroundColor: colors.dark.text }}>
+    <View style={viewStyle}>
       <View style={chapterTitleContainerStyle}>
         <Text style={chapterIndexStyle}>{chapterName}</Text>
         <Text style={chapterTitleStyle}>{name}</Text>
       </View>
-      <ScrollView>
-        <View style={chapterContainerStyle}>
-          {
+      <ScrollView contentContainerStyle={chapterContainerStyle}>
+        {
+          /**
+          * Map the places
+          */
+          places.map((place: DeckPlace, index: number) => {
+            const isEven = index % 2 === 0;
+            const rotate = isEven ? '-5deg' : '5deg';
+            const reverseRotate = isEven ? '5deg' : '-5deg';
+
             /**
-             * Map the places
+             * Destructure the place data
              */
-            places.map((place: DeckPlace, index: number) => {
-              const isEven = index % 2 === 0;
-              const rotate = isEven ? '-5deg' : '5deg';
-              const reverseRotate = isEven ? '5deg' : '-5deg';
+            const { id: placeId, name, description, image } = place;
+            const progressPercent = placeProgressById[ placeId ] ?? 0;
 
-              /**
-               * Destructure the place data
-               */
-              const { id: placeId, name, description, image } = place;
-              const progressPercent = placeProgressById[ placeId ] ?? 0;
-
-              /**
-               * Render the place view/card
-               */
-              return (
-                <View key={placeId} style={placeContainerStyle}>
-                  <View style={placeTitleContainerStyle}>
-                    <Text style={placeTitleTextStyle}>{name}</Text>
-                  </View>
-                  <View
-                    style={[ polaroidContainerStyle, { transform: [ { rotate: reverseRotate } ] } ]}
-                  >
-                    <View style={[ polaroid, { transform: [ { rotate } ] } ]}>
-                      <View style={polaroidInnerStyle}>
-                        <Image
-                          source={image}
-                          style={placeImageStyle}
+            /**
+             * Render the place view/card
+             */
+            return (
+              <View key={placeId} style={placeContainerStyle}>
+                <Text style={placeTitleTextStyle}>{name}</Text>
+                <View
+                  style={[ polaroidContainerStyle, { transform: [ { rotate: reverseRotate } ] } ]}
+                >
+                  <View style={[ polaroid, { transform: [ { rotate } ] } ]}>
+                    <Image
+                      source={image}
+                      style={placeImageStyle}
+                    />
+                    <Text style={placeDescriptionTextStyle}>{description}</Text>
+                    <View style={progressContainerStyle}>
+                      <Text style={progressTextStyle}>Progress {progressPercent}%</Text>
+                      <View style={progressBarsContainer}>
+                        <LinearGradient
+                          style={[ progressBarStyle, { width: `${progressPercent}%`, borderRightWidth: 1 } ]}
+                          colors={polaroidColors}
+                          locations={polaroidColorStops}
                         />
-                        <View>
-                          <View style={placeNameContainerStyle}>
-                            <Text style={placeDescriptionTextStyle}>{description}</Text>
-                          </View>
-                        </View>
-                        <View style={progressContainerStyle}>
-                          <Text style={progressTextStyle}>Progress {progressPercent}%</Text>
-                          <View style={progressBarsContainer}>
-                            <LinearGradient
-                              style={[ progressBarStyle, { width: `${progressPercent}%`, borderRightWidth: 1 } ]}
-                              colors={[
-                                '#E6320D', '#E6320D',
-                                '#F17E06', '#F17E06',
-                                '#F8BA26', '#F8BA26',
-                                '#78BA34', '#78BA34',
-                                '#3791CF', '#3791CF'
-                              ]}
-                              locations={[ 0, 0.2, 0.2, 0.4, 0.4, 0.6, 0.6, 0.8, 0.8, 1 ]}
-                            />
-                            <LinearGradient
-                              style={[ progressBarStyle, { position: 'absolute', width: '100%', opacity: 0.15 } ]}
-                              colors={[
-                                '#E6320D', '#E6320D',
-                                '#F17E06', '#F17E06',
-                                '#F8BA26', '#F8BA26',
-                                '#78BA34', '#78BA34',
-                                '#3791CF', '#3791CF'
-                              ]}
-                              locations={[ 0, 0.2, 0.2, 0.4, 0.4, 0.6, 0.6, 0.8, 0.8, 1 ]}
-                            />
-                          </View>
-                        </View>
+                        <LinearGradient
+                          style={[ progressBarStyle, { position: 'absolute', width: '100%', opacity: 0.15 } ]}
+                          colors={polaroidColors}
+                          locations={polaroidColorStops}
+                        />
                       </View>
                     </View>
                   </View>
-                  <LinkButton
-                    hitSlop={10}
-                    arrowSize={16}
-                    style={placeSelectButtonStyle}
-                    screen={'(routes)/CardDeckSelect'}
-                    params={{ placeId }}
-                  >
-                    <Text style={placeSelectButtonTextStyle}>View decks</Text>
-                  </LinkButton>
                 </View>
-              );
-            })
-          }
-        </View>
+                <LinkButton
+                  hitSlop={10}
+                  arrowSize={16}
+                  style={placeSelectButtonStyle}
+                  screen={'(routes)/CardDeckSelect'}
+                  params={{ placeId }}
+                >
+                  <Text style={placeSelectButtonTextStyle}>View decks</Text>
+                </LinkButton>
+              </View>
+            );
+          })
+        }
       </ScrollView>
     </View>
   );
@@ -186,16 +185,32 @@ const { containerMargin } = sharedStyles;
  * TODO: styles
  */
 const styles = StyleSheet.create({
-  chapterContainerStyle: {
-    display: 'flex',
+  viewStyle: {
+    flex: 1,
     backgroundColor: colors.dark.text,
-    gap: 8,
-    paddingBottom: 16
   },
   chapterTitleContainerStyle: {
     paddingHorizontal: containerMargin,
     paddingVertical: 16,
     backgroundColor: colors.dark.text
+  },
+  chapterIndexStyle: {
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    color: colors.light.text,
+  },
+  chapterTitleStyle: {
+    textAlign: 'center',
+    width: '100%',
+    fontFamily: 'lexend-600',
+    fontSize: 20,
+    color: colors.light.text,
+  },
+  chapterContainerStyle: {
+    display: 'flex',
+    backgroundColor: colors.dark.text,
+    gap: 8,
+    paddingBottom: 16
   },
   placeContainerStyle: {
     padding: containerMargin,
@@ -203,6 +218,12 @@ const styles = StyleSheet.create({
     marginHorizontal: 8,
     borderRadius: 8,
     backgroundColor: colors.dark.background,
+  },
+  placeTitleTextStyle: {
+    color: colors.light.text,
+    fontFamily: 'lexend-600',
+    fontSize: 18,
+    textAlign: 'center',
   },
   polaroidContainerStyle: {
     display: 'flex',
@@ -222,35 +243,7 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     borderWidth: 1,
     borderColor: colors.light.border,
-  },
-  polaroidInnerStyle: {
     padding: 12,
-  },
-  chapterIndexStyle: {
-    textAlign: 'center',
-    textTransform: 'uppercase',
-    color: colors.light.text,
-  },
-  chapterTitleStyle: {
-    textAlign: 'center',
-    width: '100%',
-    fontFamily: 'lexend-600',
-    fontSize: 20,
-    color: colors.light.text,
-  },
-  placeTitleContainerStyle: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  placeTitleTextStyle: {
-    color: colors.light.text,
-    fontFamily: 'lexend-600',
-    fontSize: 18
-  },
-  progressContainerStyle: {
-    paddingHorizontal: 8,
-    paddingTop: 4
   },
   placeImageStyle: {
     width: '100%',
@@ -259,27 +252,28 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: colors.dark.border
   },
-  placeNameContainerStyle: {
-    alignItems: 'center',
-  },
   placeDescriptionTextStyle: {
     padding: 8,
     fontFamily: 'shadows-400',
     fontSize: 16,
   },
-  progressBarStyle: {
-    width: '10%',
-    height: 8,
-    borderColor: colors.light.border
+  progressContainerStyle: {
+    paddingHorizontal: 8,
+    paddingTop: 4
+  },
+  progressTextStyle: {
+    fontFamily: 'shadows-400',
+    fontSize: 12,
   },
   progressBarsContainer: {
     overflow: 'hidden',
     borderTopWidth: 1,
     borderColor: colors.light.border,
   },
-  progressTextStyle: {
-    fontFamily: 'shadows-400',
-    fontSize: 12,
+  progressBarStyle: {
+    width: '10%',
+    height: 8,
+    borderColor: colors.light.border
   },
   placeSelectButtonStyle: {
     alignSelf: 'center',
